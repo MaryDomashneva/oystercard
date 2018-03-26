e0d04a49cb0b559bf8becbd32897c387723e9ca4
marydomashneva <mary.domashneva@gmail.com>
2018-03-26 17:50:02 +0100

    step #7 done oystercard challlenge. deducted method added to Oyster class

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,14 +1,30 @@
 class Oystercard
   attr_reader :balance
   DEFAULT_CAPACITY = 90
+  ERROR_MESSAGES = {
+    :exceeded_limit => 'The amount you are trying to top up is above limit = 90'
+  }
 
   def initialize(balance = 0)
     @balance = balance
   end
 
   def top_up(amount)
-    raise 'The amount you are trying to top up is above limit = 90' if amount > DEFAULT_CAPACITY
+    raise  ERROR_MESSAGES[:exceeded_limit] if limit_reached?(amount)
     @balance += amount
     return @balance
   end
+
+  def deduct(amount)
+    @balance -= amount
+    return @balance
+  end
+
+  private
+
+  def limit_reached?(amount)
+    @balance + amount > DEFAULT_CAPACITY
+  end
+
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,9 +1,12 @@
 require 'oystercard'
 DEFAULT_CAPACITY = 90
+ERROR_MESSAGES = {
+  :exceeded_limit => 'The amount you are trying to top up is above limit = 90'
+}
 
 describe Oystercard do
   context 'Newly initialized Oyster card has a balance by default' do
-    context 'when balance set-up to "0"' do
+    context 'initial balance set-up to "0"' do
       it 'uses that balance' do
         balance = 0
         card = Oystercard.new(0)
@@ -22,9 +25,19 @@ describe Oystercard do
 
         context 'when the amount of top_up does not fit the default capacity' do
           it 'raises an error' do
-            expect { subject.top_up(100) }.to raise_error('The amount you are trying to top up is above limit = 90')
+            expect { subject.top_up(100) }.to raise_error(ERROR_MESSAGES[:exceeded_limit])
           end
         end
+      end
+
+      context 'Oyster card can be deduct' do
+        it { expect(subject).to respond_to(:deduct).with(1).argument }
+      end
+      it 'returns new balance when deduct' do
+        amount_top_up = 80
+        subject.top_up(amount_top_up)
+        amount_deduct = 10
+        expect(subject.deduct(amount_deduct)).to eq(70)
       end
     end
   end
