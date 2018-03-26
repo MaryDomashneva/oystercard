919aebd6a767eb5d27660ce8e6987ae917819218
marydomashneva <mary.domashneva@gmail.com>
2018-03-26 17:22:33 +0100

    set a default capacity to Oystercard

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,13 @@
 class Oystercard
   attr_reader :balance
-  DEFAULT_CAPACITY = 0
+  DEFAULT_CAPACITY = 90
 
-  def initialize(balance = DEFAULT_CAPACITY)
+  def initialize(balance = 0)
     @balance = balance
   end
 
   def top_up(amount)
+    raise 'The amount you are trying to top up is above limit = 90' if amount > DEFAULT_CAPACITY
     @balance += amount
     return @balance
   end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,11 +1,11 @@
 require 'oystercard'
-DEFAULT_CAPACITY = 0
+DEFAULT_CAPACITY = 90
 
 describe Oystercard do
   context 'Newly initialized Oyster card has a balance by default' do
     context 'when balance set-up to "0"' do
       it 'uses that balance' do
-        balance = DEFAULT_CAPACITY
+        balance = 0
         card = Oystercard.new(0)
         expect(card.balance).to eq(balance)
       end
@@ -13,9 +13,17 @@ describe Oystercard do
       context 'Oyser card can be top_up' do
         it { expect(subject).to respond_to(:top_up).with(1).argument }
 
-        it 'returns new balance when topped_up' do
-          amount = 80
-          expect(subject.top_up(amount)).to eq(80)
+        context 'when the amount of top_up fit the default capacity' do
+          it 'returns new balance when topped_up' do
+            amount = 80
+            expect(subject.top_up(amount)).to eq(80)
+          end
+        end
+
+        context 'when the amount of top_up does not fit the default capacity' do
+          it 'raises an error' do
+            expect { subject.top_up(100) }.to raise_error('The amount you are trying to top up is above limit = 90')
+          end
         end
       end
     end
