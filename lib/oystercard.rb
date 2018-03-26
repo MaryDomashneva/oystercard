948bf09bf89d1d116b19a234b15c4685973f3547
marydomashneva <mary.domashneva@gmail.com>
2018-03-26 15:29:52 +0100

    Class Oystercard first test pass commit

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,2 +1,8 @@
 class Oystercard
+  attr_reader :balance
+  DEFAULT_CAPACITY = 0
+
+  def initialize(balance = DEFAULT_CAPACITY)
+    @balance = balance
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,14 @@
 require 'oystercard'
+DEFAULT_CAPACITY = 0
+
 describe Oystercard do
+  context 'Newly initialized Oyster card has a balance by default' do
+    context 'when balance set-up to "0"' do
+      it "uses that balance" do
+        balance = DEFAULT_CAPACITY
+        card = Oystercard.new(0)
+        expect(card.balance).to eq(balance)
+      end
+    end
+  end
 end
